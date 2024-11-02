a1dcc9406f12654d691e40ee66765ff9242e04ea
grace-kal <kalinina.grace@gmail.com>
2024-11-02 19:33:35 +0200

    initial commit

---
+++ ShowTime/ShowTime.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShowTime.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShowTime/ShowTime.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShowTime.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
